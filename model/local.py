3e33a97088aa498867e0e85b5de4bf6213952f4d
Matteo <matteo.cucco@hotmail.fr>
2024-01-13 14:52:45 +0100

    local.create_sensors V2 plus propre avec 1 seule reqête pour la récupération des types de capteurs

--- model/local.py
+++ model/local.py
@@ -794,45 +794,6 @@ def create_sensor_configs(id_config, sensor_list):
     """
 
 
-def get_id_type_from_label(label):  # TODO : modify this function to return a list and check in the python code to
-    # associate the correct ids to the sensor list
-    """!
-    Finds the sensor type id corresponding to the label
-
-    @param label: The label of the error
-    @return: The corresponding id_type if found, None if nothing was found or an error occurred
-    """
-    global local_db, local_cursor
-    try:
-        # Check if the local database connection is established
-        if local_db is not None and local_db.is_connected():
-            query = "SELECT id_type FROM sensor_type WHERE type = %s"
-
-            with local_cursor_protection:
-                local_cursor.execute(query, (label,))
-
-                # Fetch the result
-                result = local_cursor.fetchone()
-            if result:
-                return result[0]
-        else:
-            # If not connected to the local database, attempt to reconnect and retry
-            print(
-                "get_id_type_from_label Error while executing select statement : database is not connected, retrying")
-            connect_to_local_db()
-            return get_id_type_from_label(label)
-
-    except Exception as e:
-        # Handle any exceptions that may occur during the query execution
-        print(f"Error finding sensor type from id_type: {e}")
-        if (e.errno == 2013):
-            connect_to_local_db()
-            return get_id_type_from_label(label)
-
-    # Return None if the sensor type is not found or there are errors
-    return None
-
-
 def create_sensors(id_observation, sensor_list):
     """!
     Saves the information of one or more sensors in both databases
@@ -847,8 +808,19 @@ def create_sensors(id_observation, sensor_list):
     if id_observation is None:
         return False
 
+    types_list = get_sensor_type_list()
+
     for sensor in sensor_list:  # Go through the sensor list
-        id_type = get_id_type_from_label(sensor['type'])    # Get the corresponding id_type
+        id_type = None
+        for id, type in types_list:   # Get the corresponding id_type
+            if sensor["type"] == type:
+                id_type = id
+                break
+
+        if id_type is None:
+            print("Sensor type not found in types list")
+            return False
+
         values = (sensor["ieee_address"], id_type, id_observation, sensor["label"], sensor["description"])
 
         # Check for any errors while sending data
