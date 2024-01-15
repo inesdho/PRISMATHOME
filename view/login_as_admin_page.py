e1129c0c9e55025a8c41b11e7cc0a9149d5bdf85
Mathilde <mathilde.dumont@ig2i.centralelille.fr>
2024-01-15 16:43:40 +0100

    sauvegarder le nb de capteurs sélectionnés d'une page sur l'autre

--- view/login_as_admin_page.py
+++ view/login_as_admin_page.py
@@ -79,7 +79,6 @@ class LoginAsAdministrator:
         print(password)
 
         user = local.get_user_from_login_and_password(username, password)
-        print(user)
 
         # Check if the user was found in the database
         if user:
