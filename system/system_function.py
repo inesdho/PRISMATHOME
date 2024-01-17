34713c14e84ace53c67f4bbd3dec9bbae96c88fa
Matteo <matteo.cucco@hotmail.fr>
2024-01-17 20:11:21 +0100

    export_remote_queries V1 à tester

--- system/system_function.py
+++ system/system_function.py
@@ -1,9 +1,12 @@
 import subprocess
 import os
 # TODO faire la doc
+
+
 def send_signal(pid, signal_name):
     script_path = "/home/share/PRISMATHOME/test"
     subprocess.run([script_path, str(pid), signal_name], check=True)
+
 
 def get_pid_of_script(script_name):
     """!
@@ -32,5 +35,27 @@ def export_remote_queries(file_path, remote_queries):
     @param file_path: The path of the file to save the queries into
     @param remote_queries: The list of remote queries
     @return True if the file was successfully created and filled, 0 otherwise
-    TODO
+    TODO finish and test this function
     """
+
+    if not file_path:
+        return False
+
+    # TODO: add filename (timestamp ?) and extension
+
+    # Check if the file path exists and if not, create it
+    if not os.path.exists(file_path):
+        try:
+            os.makedirs(os.path.dirname(file_path))
+        except OSError:
+            pass
+
+
+    # Open the file in write mode
+    with open(file_path, 'w') as f:
+        try:
+            for query in remote_queries:
+                query_as_string = str(query[1])
+                f.write(query_as_string + '\n')
+        except Exception as e:
+            print(f"Error while exporting queries :", e)
